bee2ed1ada1ed5cd88bd9b20d2c3b1912e4c1e5b
fly <fly@load-page.com>
2015-09-12 15:32:43 +0800

    fix AdminTrait

--- vendor/addons/core/src/Controllers/AdminTrait.php
+++ vendor/addons/core/src/Controllers/AdminTrait.php
@@ -45,7 +45,7 @@ trait AdminTrait {
 	private function _getFilters(Request $request, Builder $builder)
 	{
 		$filters = [];
-		$inputs = $request->input('filter') ?: $request->except('_', 'of', 'base','draw','columns','order','pagesize', 'length', 'start', 'page', 'search');
+		$inputs = $request->input('filters') ?: [];//$request->except('_', 'of', 'base','draw','columns','order','pagesize', 'length', 'start', 'page', 'search');
 		foreach ($inputs as $k => $v)
 			$filters[$k] = is_array($v) ? array_change_key_case($v) : ['equal' => $v];
 
