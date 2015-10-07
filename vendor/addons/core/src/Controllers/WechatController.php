e35ca9c141f9604e13e9c31b32f45bf2b0c0a5ca
fly <fly@load-page.com>
2015-10-07 13:40:35 +0800

    fix wechat

--- vendor/addons/core/src/Controllers/WechatController.php
+++ vendor/addons/core/src/Controllers/WechatController.php
@@ -33,7 +33,7 @@ abstract class WechatController extends Controller {
 	{
 		$api = $account = null;
 		$_config = ['debug' => true, 'logcallback' => function($log, API $api){
-			WechatLog::create(['log' => $log, 'waid' => $api->waid]);
+			WechatLog::create(['log' => $log, 'waid' => $api->waid, 'url' => app('Illuminate\Routing\UrlGenerator')->current()]);
 		}];
 		if (empty($id)) //没有id，则尝试去数据库找
 		{
