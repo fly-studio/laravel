1e416209016357ed2d54771ef462bed29a3eb4a1
fly <fly@load-page.com>
2015-10-09 11:18:58 +0800

    upgrade wechat

--- vendor/addons/core/src/Jobs/WechatSend.php
+++ vendor/addons/core/src/Jobs/WechatSend.php
@@ -14,6 +14,11 @@ use Addons\Core\Models\WechatUser;
 use Addons\Core\Models\WechatDepot;
 use Addons\Core\Models\WechatTemplate;
 use Addons\Core\Models\Attachment;
+use Addons\Core\Models\WechatMessage;
+use Addons\Core\Models\WechatMessageText;
+use Addons\Core\Models\WechatMessageMedia;
+use Addons\Core\Models\WechatMessageLink;
+use Addons\Core\Models\WechatMessageLocation;
 
 class WechatSend implements SelfHandling, ShouldQueue
 {
@@ -43,20 +48,17 @@ class WechatSend implements SelfHandling, ShouldQueue
 	public function handle()
 	{
 		$api = new API($this->account->toArray(), $this->account->getKey());
-		$data = [
-			'touser' => $this->user->openid,
-		];
+		$message = WechatMessage::create(['waid' => $api->waid, 'wuid' => $this->user->getKey(), 'message_id' => '', 'type' => NULL, 'transport_type' => 'send']);
+		$data = ['touser' => $this->user->openid,];
+		$type = 'text';
 		if ($this->media instanceof Attachment)
 		{
 			$type = $this->media->file_type();
 			$type == 'audio' && $type = 'voice';
 			$path = $this->media->_create_symlink();
 			$media_id = $api->uploadMedia($path, $type);
-			
-			$data += [
-				'msgtype' => $type,
-				$type => ['media_id' => $media_id],
-			];
+
+			$data += ['msgtype' => $type, $type => ['media_id' => $media_id],];
 			switch ($type) {
 				case 'image':
 				case 'voice':
@@ -69,19 +71,27 @@ class WechatSend implements SelfHandling, ShouldQueue
 					];
 					break;
 			}
+			//入库
+			WechatMessageMedia::create(['id' => $message->getKey(), 'media_id' => $media_id, 'format' => $this->media->ext]);
 			
 			//图片、视频、
 		} elseif ($this->media instanceof WechatDepot) { //素材
 			$type = $this->media->type;
 
-			
-
+			//入库
+			$message->wdid = $this->media->getKey();
 		} else { //String
 			$data += [
 				'msgtype' => 'text',
 				'text' => ['content' => $media],
 			];
+			//入库
+			WechatMessageText::create(['id' => $message->getKey(), 'content' => $rev->getRevContent()]);
 		}
+
+		$message->type = $type;
+		$message->save();
+
 		return $api->sendCustomMessage($data);
 	}
 }
