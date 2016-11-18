daf0f21af281ba733ac9d26161b4e45fb30036ef
fly <fly@load-page.com>
2016-11-18 11:10:26 +0800

    fix

--- vendor/addons/smarty/plugins/function.pluginclude.php
+++ vendor/addons/smarty/plugins/function.pluginclude.php
@@ -28,7 +28,8 @@ function smarty_function_pluginclude($params, $template)
 	for($i = 1; $i < count($dbt);$i++)
 		if ($dbt[$i]['function'] == __FUNCTION__)
 		{
-			trigger_error("pluginclude: cannot call 'pluginclude' in 'pluginclude' (recursive)", E_USER_NOTICE);			return;
+			trigger_error("pluginclude: cannot call 'pluginclude' in 'pluginclude' (nested)", E_USER_NOTICE);
+			return;
 		}
 
 	$file = '';
