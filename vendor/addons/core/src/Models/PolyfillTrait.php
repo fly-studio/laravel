e3fdf97fac47d512e04059539b30088d81c86c3f
fly <fly@load-page.com>
2016-12-07 19:36:20 +0800

    add attributesToArray to PolyfillTrait.php

--- vendor/addons/core/src/Models/PolyfillTrait.php
+++ vendor/addons/core/src/Models/PolyfillTrait.php
@@ -44,22 +44,43 @@ trait PolyfillTrait{
 	}
 
 	/**
-     * Cast an attribute to a native PHP type.
-     *
-     * @param  string  $key
-     * @param  mixed  $value
-     * @return mixed
-     */
-    protected function castAttribute($key, $value)
-    {
-    	$type = $this->getCastType($key);
-    	if (!empty($type))
-    	{
-    		$method = 'as'.Str::studly($type);
-    		if (method_exists($this, $method))
-    			return call_user_func([$this, $method], $value);
-    	}
-    	return parent::castAttribute($key, $value);
-    }
+	 * Cast an attribute to a native PHP type.
+	 *
+	 * @param  string  $key
+	 * @param  mixed  $value
+	 * @return mixed
+	 */
+	protected function castAttribute($key, $value)
+	{
+		$type = $this->getCastType($key);
+		if (!empty($type) && !in_array($type, ['int','integer','real','float','double','string','bool','boolean','object','array','json','collection','date','datetime','timestamp']))
+		{
+			$method = 'as'.Str::studly($type);
+			if (method_exists($this, $method))
+				return call_user_func([$this, $method], $value);
+		}
+		return parent::castAttribute($key, $value);
+	}
+
+	/**
+	 * Convert the model's attributes to an array.
+	 *
+	 * @return array
+	 */
+	public function attributesToArray()
+	{
+		$data = parent::attributesToArray();
+		foreach ($this->getCasts() as $key => $type)
+		{
+			//$type = $this->getCastType($key);
+			if (!empty($type) && !in_array($type, ['int','integer','real','float','double','string','bool','boolean','object','array','json','collection','date','datetime','timestamp']))
+			{
+				$method = 'from'.Str::studly($type);
+				if (method_exists($this, $method))
+					$data[$key] = call_user_func([$this, $method], $data[$key]);
+			}
+		}
+		return $data;
+	}
 
 }
