609f0132b82d86d3a70070853a17d80bcebb2d6f
fly <fly@load-page.com>
2016-12-13 16:22:08 +0800

    fix laravel/scout suppot elasticsearch 5.x

--- vendor/addons/elasticsearch/src/ServiceProvider.php
+++ vendor/addons/elasticsearch/src/ServiceProvider.php
@@ -2,6 +2,7 @@
 namespace Addons\Elasticsearch;
 
 use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder as Elasticsearch;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 
@@ -13,49 +14,56 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 class ServiceProvider extends BaseServiceProvider
 {
 
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $configPath = realpath(__DIR__ . '/../config/elasticsearch.php');
-        $this->publishes([
-            $configPath => config_path('elasticsearch.php'),
-        ]);
-    }
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$configPath = realpath(__DIR__ . '/../config/elasticsearch.php');
+		$this->publishes([
+			$configPath => config_path('elasticsearch.php'),
+		]);
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $app = $this->app;
+		app(\Laravel\Scout\EngineManager::class)->extend('elasticsearch', function(){
+			return new AdvancedElasticsearchEngine(
+				Elasticsearch::fromConfig(config('scout.elasticsearch.config')),
+				config('scout.elasticsearch.index')
+			);
+		});
+	}
 
-        $this->mergeConfigFrom(__DIR__ . '/../config/elasticsearch.php', 'elasticsearch');
-        config('elasticsearch.connections.default.logObject') === 'monolog' && config(['elasticsearch.connections.default.logObject' => $app->make('log')]);
-        config('scout.elasticsearch.config.logger') === 'monolog' && config(['scout.elasticsearch.config.logger' => $app->make('log')]);
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$app = $this->app;
 
-        $app->singleton('elasticsearch.factory', function($app) {
-            return new Factory();
-        });
+		$this->mergeConfigFrom(__DIR__ . '/../config/elasticsearch.php', 'elasticsearch');
+		config('elasticsearch.connections.default.logObject') === 'monolog' && config(['elasticsearch.connections.default.logObject' => $app->make('log')]);
+		config('scout.elasticsearch.config.logger') === 'monolog' && config(['scout.elasticsearch.config.logger' => $app->make('log')]);
 
-        $app->singleton('elasticsearch', function($app) {
-            return new Manager($app, $app['elasticsearch.factory']);
-        });
+		$app->singleton('elasticsearch.factory', function($app) {
+			return new Factory();
+		});
 
-        $app->singleton(Client::class, function($app) {
-            return $app['elasticsearch']->connection();
-        });
-    }
+		$app->singleton('elasticsearch', function($app) {
+			return new Manager($app, $app['elasticsearch.factory']);
+		});
+
+		$app->singleton(Client::class, function($app) {
+			return $app['elasticsearch']->connection();
+		});
+	}
 }
