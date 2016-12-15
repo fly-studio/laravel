b2f4896695bf68b6f71227ee66d60143304984af
fly <fly@load-page.com>
2016-12-15 19:24:51 +0800

    add logstash

--- vendor/addons/elasticsearch/src/ServiceProvider.php
+++ vendor/addons/elasticsearch/src/ServiceProvider.php
@@ -4,7 +4,10 @@ namespace Addons\Elasticsearch;
 use Elasticsearch\Client;
 use Elasticsearch\ClientBuilder as Elasticsearch;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
-
+use Monolog\Formatter\LogstashFormatter;
+use Monolog\Handler\ElasticSearchHandler;
+use Monolog\Handler\RedisHandler;
+use Monolog\Handler\RotatingFileHandler;
 
 /**
  * Class ServiceProvider
@@ -33,12 +36,42 @@ class ServiceProvider extends BaseServiceProvider
 			$configPath => config_path('elasticsearch.php'),
 		]);
 
+		//the scout/ElasticsearchEngine supported elasticsearch 2.x, fix it
 		app(\Laravel\Scout\EngineManager::class)->extend('elasticsearch', function(){
 			return new AdvancedElasticsearchEngine(
 				Elasticsearch::fromConfig(config('scout.elasticsearch.config')),
 				config('scout.elasticsearch.index')
 			);
 		});
+
+		$type = app('elasticsearch')->getConfig('index');
+		$index = 'logstash-'.$type;
+		switch (app('elasticsearch')->getConfig('logstashDriver'))
+		{
+			case 'file':
+				$handler = new RotatingFileHandler(
+		            storage_path('/logs/logstash.log'),
+		            config('app.log_max_files', 5),
+		            config('app.log_level', 'debug')
+		        );
+	            $handler->setFormatter(new LogstashFormatter($type, null, null, 'ctxt_', LogstashFormatter::V1));
+				break;
+			case 'redis':
+				$handler = new RedisHandler(app('redis')->connection(), $index);
+	            $handler->setFormatter(new LogstashFormatter($type, null, null, 'ctxt_', LogstashFormatter::V1));
+				break;
+			case 'elasticsearch':
+				// laravel's log write to Logstash
+				$config = [
+					'index' => $index,
+					'type' => $type
+				];
+				$handler = new ElasticSearchHandler(app(Client::class), $config);
+				break;
+				
+		}
+		!empty($handler) && app('log')->getMonolog()->pushHandler($handler);
+		
 	}
 
 	/**
