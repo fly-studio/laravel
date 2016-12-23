36d00ee6ba26045ecae74fc7e5e603a8e4735a3d
fly <fly@load-page.com>
2016-12-24 00:41:43 +0800

    upgrade elasticsearch/scout

--- vendor/addons/elasticsearch/src/Scout/Searchable.php
+++ vendor/addons/elasticsearch/src/Scout/Searchable.php
@@ -8,46 +8,49 @@ trait Searchable {
 	use BaseSearchable;
 
 	/**
-     * Perform a search against the model's indexed data.
-     * @example
-     * $query = [
-     *     [
-     *         'term' => [
-     *             'name' => 'admin'
-     *         ]
-     *     ],
-     *     [
-     *         'multi_match' => [
-     *             'fields' => ['name', 'title'],
-     *             'query' => 'admin'
-     *         ]
-     *     ] 
-     * ];
-     * search($query);
-     * 
-     * @example
-     * search('admin');
-     *
-     * @example
-     * $query = [
-     *     'term' => [
-     *         'name' => 'admin'
-     *     ]
-     * ];
-     * search($query);
-     * 
-     *
-     * @param  string|array  $query
-     * @param  Closure  $callback
-     * @return \Addons\Elasticsearch\Scout\Builder
-     */
-    public static function search($query = null, $callback = null)
-    {
-        $builder = new Builder(new static, null, $callback);
-        if (is_null($query))
-            !is_array($query) ? $builder->where('_all', $query) : $builder->where($query);
+	 * Perform a search against the model's indexed data.
+	 * @example 
+	 * search()->where(...)->get()
+	 * search(...)->keys()
+	 * search(...)->where(...)->count()
+	 * @example _all matach
+	 * search('admin'); like where('_all', 'keywords');
+	 * @example sequential numerical array list
+	 * $query = [
+	 *     [
+	 *         'term' => [
+	 *             'name' => 'admin'
+	 *         ]
+	 *     ],
+	 *     [
+	 *         'multi_match' => [
+	 *             'fields' => ['name', 'title'],
+	 *             'query' => 'admin'
+	 *         ]
+	 *     ] 
+	 * ];
+	 * search($query); like where($query);
+	 * @example associative array
+	 * $query = [
+	 *     'term' => [
+	 *         'name' => 'admin'
+	 *     ]
+	 * ];
+	 * search($query); like where($query);
+	 * 
+	 *
+	 * @param  string|array  $_allOrCondition _all keywords or a array
+	 * @param  string        $bool [must]|should|filter|must_not
+	 * @param  Closure       $callback
+	 * @return \Addons\Elasticsearch\Scout\Builder
+	 */
+	public static function search($_allOrCondition = null, $bool = 'must', $callback = null)
+	{
+		$builder = new Builder(new static, $bool, $callback);
+		if (!is_null($_allOrCondition))
+			!is_array($_allOrCondition) ? $builder->whereAll($_allOrCondition) : $builder->where($_allOrCondition);
 
-        return $builder;
-    }
+		return $builder;
+	}
 
 }
