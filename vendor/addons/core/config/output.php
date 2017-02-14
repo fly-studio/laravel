0b8df6fe1324f04c87f55a4d3bb26ade6f7a3c44
fly <fly@load-page.com>
2017-02-15 00:36:09 +0800

    add output tip manger

---
+++ vendor/addons/core/config/output.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	'tips' => [
+		'redirect' => [
+			'timeout' => 1500,
+		],
+		'back' => [
+			'timeout' => false,
+		],
+		'refresh' => [
+			'timeout' => 1500,
+		],
+		'toast' => [
+			'timeout' => 2500,
+		],
+	], 
+];
---
+++ vendor/addons/core/src/Http/Output/Tips/Refresh.php
@@ -0,0 +1,3 @@
+<?php
+
+namespace Addons\Core\Http\Output\Tips;
