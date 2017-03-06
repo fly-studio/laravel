85c07ffc7aecb2d1a7a81ed042ac5f6f9020a622
fly <fly@load-page.com>
2017-03-06 12:55:59 +0800

    fix

---
+++ vendor/addons/core/src/Middleware/NoCache.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Addons\Core\Middleware;
+
+use Closure;
+use Illuminate\Http\Response;
+
+class NoCache
+{
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		$response = $next($request);
+		
+		$response->headers->set('P3P','CP="CAO PSA OUR"');
+		if(in_array($request->method(), ['POST', 'PUT', 'DELETE'])){
+			//header no cache when post
+			foreach([
+				'Expires' => '0',
+				'Cache-Control' => 'no-store,private, post-check=0, pre-check=0, max-age=0',
+				'Pragma' => 'no-cache',
+			] as $k => $v)
+				$response->headers->set($k, $v);
+		}
+
+		return $response;
+	}
+}
