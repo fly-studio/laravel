b00c91a3094a760e836a94ebbe726f7ac32bdf44
fly <fly@load-page.com>
2017-03-07 12:27:02 +0800

    fix cache in model

--- vendor/addons/core/src/Models/CacheTrait.php
+++ vendor/addons/core/src/Models/CacheTrait.php
@@ -43,9 +43,11 @@ trait CacheTrait{
 		$key = $model->getTable().','.$min.'-'.$max;
 		$data = Cache::remember($key, config('cache.ttl'), function() use($min, $max, $keyName){ // 7 days
 			$models = static::where($keyName, '>=', $min * 1000)->where($keyName, '<', $max * 1000)->get();
-			return $models->keyBy($keyName);
+			return $models->mapWithKeys(function($model, $key){
+				return [$model->getKey() => $model->getAttributes()];
+			});
 		});
-		return $data->get($id);
+		return $data->has($id) ? (new static)->setRawAttributes($data->get($id)) : false;
 	}
 
 }
