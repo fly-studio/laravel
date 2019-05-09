641ad051def7ae39e74ab6e8408d47f1646cd592
fly <fly@load-page.com>
2019-05-09 15:54:06 +0800

    add $columns to Repository.php

--- vendor/addons/core/src/Contracts/Repository.php
+++ vendor/addons/core/src/Contracts/Repository.php
@@ -10,10 +10,10 @@ abstract class Repository {
 	use ApiTrait;
 
 	abstract public function prePage();
-	abstract public function find($id, $columns = ['*']);
+	abstract public function find($id, array $columns = ['*']);
 	abstract public function store(array $data);
 	abstract public function update(Model $model, array $data);
 	abstract public function destroy(array $ids);
-	abstract public function data(Request $request, $columns = ['*']);
-	abstract public function export(Request $request, $columns = ['*']);
+	abstract public function data(Request $request, callable $callback = null, array $columns = ['*']);
+	abstract public function export(Request $request, callable $callback = null, array $columns = ['*']);
 }
