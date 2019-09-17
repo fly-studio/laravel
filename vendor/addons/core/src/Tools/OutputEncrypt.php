1b664265432a45a072651e99c4e90bc58e25ed42
fly <fly@load-page.com>
2019-09-17 17:41:58 +0800

    optimize OutputEncrypt

--- vendor/addons/core/src/Tools/OutputEncrypt.php
+++ vendor/addons/core/src/Tools/OutputEncrypt.php
@@ -11,7 +11,7 @@ class OutputEncrypt {
 	private $privateKey;
 	private $aesKey;
 
-	public function __construct($publicKey, $privateKey = null)
+	public function __construct(string $publicKey = null, string $privateKey = null)
 	{
 		$this->publicKey = $publicKey;
 		$this->privateKey = $privateKey;
@@ -19,9 +19,9 @@ class OutputEncrypt {
 
 	public function encodeByPublic(string $value, string $aesKey = null)
 	{
-		$encoded = $this->encodeAesToRaw($value, $aesKey);
+		$encoded = $this->encryptAesToRaw($value, $aesKey);
 
-		$aesEncrypted = $this->makePublicRsa()->encrypt($encoded['aesBase64']);
+		$aesEncrypted = $this->encryptPublicRsa($encoded['aesBase64']);
 
 		return [
 			'aesEncrypted' => !empty($aesEncrypted) ? base64_encode($aesEncrypted) : null,
@@ -31,9 +31,9 @@ class OutputEncrypt {
 
 	public function encodeByPrivate(string $value, string $aesKey = null)
 	{
-		$encoded = $this->encodeAesToRaw($value, $aesKey);
+		$encoded = $this->encryptAesToRaw($value, $aesKey);
 
-		$aesEncrypted = $this->makePrivateRsa()->encrypt($encoded['aesBase64']);
+		$aesEncrypted = $this->encryptPrivateRsa($encoded['aesBase64']);
 
 		return [
 			'aesEncrypted' => !empty($aesEncrypted) ? base64_encode($aesEncrypted) : null,
@@ -43,22 +43,22 @@ class OutputEncrypt {
 
 	public function decodeByPublic(string $value, string $aesEncrypted)
 	{
-		$decoded = $this->makePublicRsa()->decrypt(base64_decode($aesEncrypted));
+		$decoded = $this->decryptPublicRsa(base64_decode($aesEncrypted));
 
 		if (empty($decoded))
 			return null;
 
-		return $this->decodeAesFromRaw($value, $decoded);
+		return $this->decryptAesFromRaw($value, $decoded);
 	}
 
 	public function decodeByPrivate(string $value, string $aesEncrypted)
 	{
-		$decoded = $this->makePrivateRsa()->decrypt(base64_decode($aesEncrypted));
+		$decoded = $this->decryptPrivateRsa(base64_decode($aesEncrypted));
 
 		if (empty($decoded))
 			return null;
 
-		return $this->decodeAesFromRaw($value, $decoded);
+		return $this->decryptAesFromRaw($value, $decoded);
 	}
 
 	private function makePrivateRsa()
@@ -81,21 +81,40 @@ class OutputEncrypt {
 		return $rsa;
 	}
 
-	public function encodeAes(string $value, string $aesKey = null)
+	public function encryptPrivateRsa(string $value)
+	{
+		return $this->makePrivateRsa()->encrypt($value);
+	}
+
+	public function decryptPrivateRsa(string $value)
+	{
+		return $this->makePrivateRsa()->decrypt($value);
+	}
+
+	public function encryptPublicRsa(string $value)
+	{
+		return $this->makePublicRsa()->encrypt($value);
+	}
+	public function decryptPublicRsa(string $value)
+	{
+		return $this->makePublicRsa()->decrypt($value);
+	}
+
+	public function encryptAes(string $value, string $aesKey = null)
 	{
 		$e = new Encrypter($aesKey ?? $this->aesKey, config('app.cipher'));
 
 		return $e->encrypt($value, false);
 	}
 
-	public function decodeAes(string $value, string $iv, string $mac, string $aesKey = null)
+	public function decryptAes(string $value, string $iv, string $mac, string $aesKey = null)
 	{
 		$e = new Encrypter($aesKey ?? $this->aesKey, config('app.cipher'));
 
 		return $e->decrypt(compact('value', 'iv', 'mac'), false);
 	}
 
-	private function encodeAesToRaw(string $value, string $aesKey = null)
+	private function encryptAesToRaw(string $value, string $aesKey = null)
 	{
 		$aes = [
 			'iv' => '',
@@ -103,7 +122,7 @@ class OutputEncrypt {
 			'key' => $aesKey ?? $this->generateAesKey()
 		];
 
-		$encoded = $this->encodeAes($value, $aes['key']);
+		$encoded = $this->encryptAes($value, $aes['key']);
 
 		$aes['iv'] = $encoded['iv'];
 		$aes['mac'] = $encoded['mac'];
@@ -114,12 +133,12 @@ class OutputEncrypt {
 		return ['aesBase64' => $aesBase64, 'value' => $encoded['value']];
 	}
 
-	private function decodeAesFromRaw(string $value, string $decoded)
+	private function decryptAesFromRaw(string $value, string $decoded)
 	{
 		$aes = json_decode($decoded, true);
 		$aes['key'] = base64_decode($aes['key']);
 
-		return $this->decodeAes(base64_decode($value), $aes['iv'], $aes['mac'], $aes['key']);
+		return $this->decryptAes(base64_decode($value), $aes['iv'], $aes['mac'], $aes['key']);
 	}
 
 	public function generateAesKey()
